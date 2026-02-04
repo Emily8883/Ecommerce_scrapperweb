b274263dc6d08df5bca9ae2ccd09f5db9761e9f7
DevsmileGod <godsmilerxiaomin@proton.me>
2026-02-04 12:03:26 -0300

    REFACTOR: Updating the context_message to the main function in ./Gemini.py

--- Gemini.py
+++ Gemini.py
@@ -399,6 +399,13 @@ def main():
 
     input_data = read_input_file(INPUT_FILE)  # Read the input file
 
+    #  Setup the context message
+    context_message = f"""
+    Hi, Gemini.
+    
+    {input_data}
+    """
+
     finish_time = datetime.datetime.now()  # Get the finish time of the program
     print(
         f"{BackgroundColors.GREEN}Start time: {BackgroundColors.CYAN}{start_time.strftime('%d/%m/%Y - %H:%M:%S')}\n{BackgroundColors.GREEN}Finish time: {BackgroundColors.CYAN}{finish_time.strftime('%d/%m/%Y - %H:%M:%S')}\n{BackgroundColors.GREEN}Execution time: {BackgroundColors.CYAN}{calculate_execution_time(start_time, finish_time)}{Style.RESET_ALL}"
